23769d965f88773e9d8e3a6b60934756417b94da
Lais <laisrz@yahoo.com.br>
2024-04-18 19:55:07 -0300

    feat(register): add form and style to it

--- base/forms.py
+++ base/forms.py
@@ -1,9 +1,54 @@
 from django import forms
 from base.models import Customer
 from localflavor.br.forms import BRStateSelect, BRCPFField, BRZipCodeField
+from django.forms import ModelForm, TextInput, EmailInput, PasswordInput
 
 class RegisterForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    def __init__(self, *args, **kwargs):
+        kwargs["label_suffix"] = ""
+        super().__init__(*args, **kwargs)
+    password = forms.CharField(
+        label='*Senha',
+        widget=forms.PasswordInput(
+            attrs={
+                'class': "form-control", 
+                'style': 'max-width: 400px; margin: 8px -2px',
+                'placeholder': 'Senha'
+                }))
     class Meta:
         model = Customer
         fields = ['email', 'cpf', 'first_name', 'last_name', 'password', 'phone']
+        widgets = {
+            'cpf': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 400px; margin: 10px -2px',
+                'placeholder': 'CPF',
+                }),
+            'email': EmailInput(attrs={
+                'class': "form-control", 
+                'style': 'max-width: 400px; margin: 10px -2px',
+                'placeholder': 'Email'
+                }),
+            'first_name': TextInput(attrs={
+                'class': "form-control", 
+                'style': 'max-width: 400px; margin: 10px -2px',
+                'placeholder': 'Nome'
+                }),
+            'last_name': TextInput(attrs={
+                'class': "form-control", 
+                'style': 'max-width: 400px; margin: 10px -2px',
+                'placeholder': 'Sobrenome'
+                }),
+            'phone': TextInput(attrs={
+                'class': "form-control", 
+                'style': 'max-width: 400px; margin-top: 10px; margin-left: -2px',
+                'placeholder': 'Telefone'
+                })
+        }
+        labels = {
+            'email': '*Email',
+            'cpf': '*CPF',
+            'first_name': '*Nome',
+            'last_name': '*Sobrenome',
+            'phone': '*Telefone'
+        }
